2e52d5c7419aaaf207e9c244c0a89ca40e7d5873
Gladys Maniago <gfmaniago@gmail.com>
2015-10-23 21:33:21 -0500

    initialize parse, working

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Backbone = require('backbone');
+window.$ = require('jquery');
+window.jQuery = $;
+
+Parse.initialize("bvFK2Tj2CFdcww2SHnZSUfaoTCY2SVGtford92Pq", "Rmj7rSWdCLkzmD6oV0ihcHLAN6g8taOGbRDn5n0c");
